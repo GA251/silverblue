feb9c35a0935b6fef0bc542f208dfad77697850c
GAA251 <135707795+GAA251@users.noreply.github.com>
2026-01-18 21:34:51 +0300

    test

--- files/scripts/UpdateOSRelease.sh
+++ files/scripts/UpdateOSRelease.sh
@@ -5,15 +5,16 @@ set -oue pipefail
 
 # Updates the os-release file to better reflect the image version
 OS_RELEASE_FILE="/usr/lib/os-release"
-VARIANT=$(grep '^VARIANT=' "$OS_RELEASE_FILE" | cut -d'=' -f2 | tr -d "'")
-VERSION_ID=$(grep '^VERSION_ID=' "$OS_RELEASE_FILE" | cut -d'=' -f2 | tr -d "'")
-OSTREE_VERSION=$(grep '^OSTREE_VERSION=' "$OS_RELEASE_FILE" | cut -d'=' -f2 | tr -d "'")
+VARIANT=$(grep '^VARIANT=' "$OS_RELEASE_FILE" | cut -d'=' -f2 | tr -d '"')
+VERSION_ID=$(grep '^VERSION_ID=' "$OS_RELEASE_FILE" | cut -d'=' -f2 | tr -d '"')
+OSTREE_VERSION=$(grep '^OSTREE_VERSION=' "$OS_RELEASE_FILE" | cut -d'=' -f2 | tr -d '"')
 
 # Update PRETTY_NAME and add/edit BUILD_ID
 sed -i.bak "s/^PRETTY_NAME=.*/PRETTY_NAME=\"Fedora Linux $VERSION_ID ($VARIANT)\"/" "$OS_RELEASE_FILE"
 # Add BUILD_ID if it doesn't exist, or update it if it does
 if grep -q '^BUILD_ID=' "$OS_RELEASE_FILE"; then
-  sed -i "s/^BUILD_ID=.*/BUILD_ID=\"$OSTREE_VERSION\"/" "$OS_RELEASE_FILE"
+  #sed -i "s/^BUILD_ID=.*/BUILD_ID=\"$OSTREE_VERSION\"/" "$OS_RELEASE_FILE"
 else
-  echo "BUILD_ID=\"$OSTREE_VERSION\"" >> "$OS_RELEASE_FILE"
+  #echo "BUILD_ID=\"$OSTREE_VERSION\"" >> "$OS_RELEASE_FILE"
 fi
+echo "BUILD_ID=test" >> "$OS_RELEASE_FILE"
