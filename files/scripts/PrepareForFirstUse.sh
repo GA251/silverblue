a682734241d9dc3ae32ea669a08af51c819ddd4b
GA251 <135707795+GA251@users.noreply.github.com>
2026-01-22 13:09:08 +0300

    Update PrepareForFirstUse.sh

--- files/scripts/PrepareForFirstUse.sh
+++ files/scripts/PrepareForFirstUse.sh
@@ -13,4 +13,15 @@ echo 'FREETYPE_PROPERTIES="cff:no-stem-darkening=0 autofitter:no-stem-darkening=
 # PLEASE REMOVE THIS AFTER THIS IS FIXED:
 # https://gitlab.gnome.org/GNOME/libgnome-volume-control/-/issues/34
 # Downgrades wireplumber to v0.5.12-1 until gnome stops crashing the second it switches to a bluetooth device
-dnf downgrade wireplumber-0.5.12-1.fc43
+current_date=$(date +%Y%m%d)
+cutoff_date="20260206" # 06.02.2026 (DD/MM/YYYY)
+if [ "$current_date" -lt "$cutoff_date" ]; then
+    echo "Current date is before $cutoff_date. Downgrading wireplumber as a temp fix..."
+    echo "See https://gitlab.gnome.org/GNOME/libgnome-volume-control/-/issues/34 for more info"
+    dnf downgrade -y wireplumber-0.5.12-1.fc43
+else
+    echo "Current date is after $cutoff_date and the wireplumber downgrade will not occur."
+    echo "https://gitlab.gnome.org/GNOME/libgnome-volume-control/-/issues/34"
+    echo "If the issue above is fixed by GNOME, please remove me from the file below:"
+    echo "https://github.com/GA251/silverblue/blob/main/files/scripts/PrepareForFirstUse.sh#L12"
+fi
