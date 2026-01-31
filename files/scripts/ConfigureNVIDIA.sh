a2452b826248dddc56ba925938e21e4113b851a2
GA251 <135707795+GA251@users.noreply.github.com>
2026-02-01 00:24:18 +0300

    Delete files/scripts/ConfigureNVIDIA.sh

--- files/scripts/ConfigureNVIDIA.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-set -oue pipefail
-
-# Configure libva-nvidia-driver, an addon used to enable accelerated video decoding on firefox
-# Because NVIDIA couldn't be bothered to support it
-echo 'LIBVA_DRIVER_NAME=nvidia' >> /etc/environment
-echo 'CUDA_DISABLE_PERF_BOOST=1' >> /etc/environment
